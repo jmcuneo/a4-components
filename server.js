911483484ddf32931e522eb4480cde7da6b081d2
Seth Frank <sbfrank@wpi.edu>
2024-04-04 19:52:02 -0400

    Add completed using svelte

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from  'express'
 import ViteExpress from 'vite-express'
 
+const app = express()
 
 
-// const todos = [
-//   { name:'buy groceries', completed:false }
-// ]
+
+const todos = [
+  { name:'buy groceries', completed:false }
+]
+
+const taskData = [
+  {_id: 1, task: "Lab 1", class: "ECE 3849", duedate: "2024-04-05", importance: "Yes", priority: 1}
+]
 
 app.use( express.json() )
 
@@ -17,11 +23,36 @@ app.use( (req,res, next) => {
 // this will most likely be 'build' or 'public'
 //app.use( express.static( 'dist' ) )
 
-app.get( '/read', ( req, res ) => res.json( todos ) )
+app.get( '/read', ( req, res ) => res.json( taskData ) )
 
 app.post( '/add', ( req,res ) => {
-  todos.push( req.body )
-  res.json( todos )
+  let dataString = ""
+  req.on( "data", function( data ) {
+      dataString += data 
+  })
+  req.on( "end", function() {
+    let taskObj = JSON.parse( dataString );
+
+    // Update id
+    if(taskData.length == 0) {
+      taskObj._id = 1;
+    } else {
+      taskObj._id = taskData[taskData.length-1]._id + 1;
+    }
+    
+    determinePriority(taskObj);
+
+    taskData.push(taskObj);
+    res.json( taskData );
+  })
+})
+
+
+app.post( '/delete', function( req,res ) {
+  const idx = todos.findIndex( v => v.name === req.body.name )
+  todos[ idx ].completed = req.body.completed
+  
+  res.sendStatus( 200 )
 })
 
 app.post( '/change', function( req,res ) {
@@ -30,5 +61,39 @@ app.post( '/change', function( req,res ) {
   
   res.sendStatus( 200 )
 })
+
+
+
+
+
+
+
+
+
+
+// Determines the priority based on duedate, importance, and the current date
+function determinePriority(data) {
+  let currentDate = new Date();
+  // Make duedate a Date object
+  let dueDate = new Date(data.duedate);
+
+  // Convert both dates to UTC
+  let utcDate1 = Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+  let utcDate2 = Date.UTC(dueDate.getFullYear(), dueDate.getMonth(), dueDate.getDate());
+
+  // Calculate different in ms and then convert to days
+  let diffDays = Math.floor(Math.abs(utcDate2 - utcDate1) / (1000 * 60 * 60 * 24));
+
+  // Determine priority
+  if((diffDays <= 2 && data.importance === "Yes") || (diffDays <= 1 && data.importance === "No")) {
+    data.priority = 1;
+  } else if((diffDays <= 3 && data.importance === "Yes") || (diffDays <= 2 && data.importance === "No")) {
+    data.priority = 2;
+  } else if((diffDays <= 4 && data.importance === "Yes") || (diffDays <= 3 && data.importance === "No")) {
+    data.priority = 3;
+  } else {
+    data.priority = 4;
+  }
+}
 
 ViteExpress.listen( app, 3000 )
--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,15 @@
   import Counter from './lib/Counter.svelte'
 
 
+  let taskValue = "";
+  let classValue = "";
+  let duedateValue = "";
+  let importanceValue = "Yes";
 
-  const getTodos = function() {
+
+
+
+  const getTasks = function() {
     const p = fetch( '/read', {
       method:'GET' 
     })
@@ -19,17 +26,17 @@
     return p
   }
 
-  const addTodo = function( e ) {
-    const todo = document.querySelector('input').value
-    promise = fetch( '/add', {
-      method:'POST',
-      body: JSON.stringify({ name:todo, completed:false }),
-      headers: { 'Content-Type': 'application/json' }
-    })
-    .then( response => response.json() )
+
+  const addTask = function( e ) {
+    let json = {_id: -1, task: taskValue, class: classValue, duedate: duedateValue, importance: importanceValue, priority: 0};
+    const body = JSON.stringify( json );
+    promise = fetch( "/add", {
+      method:"POST",
+      body
+    }).then( response => response.json())
   }
 
-  const toggle = function( e ) {
+  const editTask = function( e ) {
     fetch( '/change', {
       method:'POST',
       body: JSON.stringify({ name:e.target.getAttribute('todo'), completed:e.target.checked }),
@@ -37,7 +44,16 @@
     })
   }
 
-  let promise = getTodos()
+
+  const deleteTask = function( e ) {
+    fetch( '/delete', {
+      method:'POST',
+      body: JSON.stringify({ name:e.target.getAttribute('todo'), completed:e.target.checked }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
+
+  let promise = getTasks()
 </script>
 
 
@@ -71,24 +87,100 @@
 
 
 
+  <h2>Add Tasks</h2>
+  <form>
+    <div class="form-group row">
+      <label for="task" class="col-sm-2 col-form-label">Enter task:</label>
+      <div class="col-sm-10">
+        <input class="form-control" type="text" id="task" placeholder="Task" bind:value={taskValue}>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="class" class="col-sm-2 col-form-label">Enter class:</label>
+      <div class="col-sm-10">
+        <input class="form-control" type="text" id="class" placeholder="Class" bind:value={classValue}>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="duedate" class="col-sm-2 col-form-label">Enter due date:</label>
+      <div class="col-sm-10">
+        <input class="form-control" type="date" id="duedate" name="Due Date" bind:value={duedateValue}>
+      </div>
+    </div>
+    <div class="row">
+      <legend class="col-form-label col-sm-2 pt-0">Choose importance:</legend>
+      <div class="col-sm-10">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="importanceRadios" id="importanceYes" value="Yes" bind:group={importanceValue} checked>
+          <label class="form-check-label" for="importanceYes">
+            Yes
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="importanceRadios" id="importanceNo" value="No" bind:group={importanceValue}>
+          <label class="form-check-label" for="importanceNo">
+            No
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-sm-10">
+        <button type="submit" class="btn btn-primary" id="submit-button" on:click={addTask}>Submit</button>
+      </div>
+    </div>
+  </form>
 
 
 
-  <input type='text' />
-  <!-- svelte-ignore missing-declaration -->
+
+
+  <h2>View Tasks</h2>
+  {#await promise then taskData}
+  <table class="table table-striped" id="data-table">
+    <thead>
+      <tr>
+        <th>Task</th>
+        <th>Class</th>
+        <th>Due Date</th>
+        <th>Important</th>
+        <th>Priority</th>
+        <th>Edit</th>
+      </tr>
+    </thead>
+    <tbody id="data-tbody">
+      {#each taskData as taskObj}
+      <tr>
+        <td>{taskObj.task}</td>
+        <td>{taskObj.class}</td>
+        <td>{taskObj.duedate}</td>
+        <td>{taskObj.importance}</td>
+        <td>{taskObj.priority}</td>
+        <td>
+          <button type="button" class="button" value="Edit">Edit</button>
+          <button type="button" class="button" value="Delete">Delete</button>
+        </td>
+      </tr>
+      {/each}
+    </tbody>
+  </table>
+  {/await}
+
+
+
+
+  <!-- <input type='text' />
   <button on:click={addTodo}>add todo</button>
 
-  <!-- svelte-ignore missing-declaration -->
   {#await promise then todos}
     <ul>
 
     {#each todos as todo}
-      <!-- svelte-ignore missing-declaration -->
       <li>{todo.name} : <input type='checkbox' todo={todo.name} checked={todo.completed} on:click={toggle}></li>
     {/each}
 
     </ul>
-  {/await}
+  {/await} -->
 
 
 
