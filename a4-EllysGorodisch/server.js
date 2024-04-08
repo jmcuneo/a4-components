c9005eb9160f11be68bc6de75263003ea74e0a67
TheDarkThatBarks <125921991+TheDarkThatBarks@users.noreply.github.com>
2024-04-08 19:10:15 -0400

    Pretty much done

--- a4-EllysGorodisch/server.js
+++ a4-EllysGorodisch/server.js
@@ -1,26 +1,249 @@
-import express from  'express'
-import ViteExpress from 'vite-express'
+import express from 'express';
+import ViteExpress from 'vite-express';
+import dotenv from 'dotenv';
+import passport from 'passport';
+import passport_github from 'passport-github2';
+import session from 'cookie-session';
+import MongoClient from 'mongodb';
+import ejs from 'ejs';
 
-const app = express()
+dotenv.config();
 
-const todos = [
-  { name:'buy groceries', completed:false }
-]
+const app = express();
 
-app.use( express.json() )
+const router = express.Router();
+const GitHubStrategy = passport_github.Strategy;
 
-app.get( '/read', ( req, res ) => res.json( todos ) )
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+      
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`;
+const client = new MongoClient.MongoClient(uri);
 
-app.post( '/add', ( req,res ) => {
-  todos.push( req.body )
-  res.json( todos )
-})
+let collection = null;
+let users = null;
 
-app.post( '/change', function( req,res ) {
-  const idx = todos.findIndex( v => v.name === req.body.name )
-  todos[ idx ].completed = req.body.completed
-  
-  res.sendStatus( 200 )
-})
+async function run() {
+    await client.connect();
+    collection = await client.db("a3-EllysGorodisch").collection("Recipes");
+    users = await client.db("a3-EllysGorodisch").collection("Users");
+    console.log("Done!");
+}
+
+run();
+
+const logger = (req, res, next) => {
+    console.log("url:", req.url);
+    next();
+};
+
+app.use(express.static("public"));
+//app.set("views", __dirname + "/public");
+app.use(logger);
+app.use(express.json());
+
+// Cookie Session
+app.use(session({
+    maxAge: 24 * 60 * 60 * 1000, // One day in milliseconds
+    keys: [process.env.COOKIE_KEY]
+}));
+
+// Regenerate Function
+app.use(function(req, res, next) {
+    if (req.session && !req.session.regenerate) {
+        req.session.regenerate = (callback) => {
+            callback();
+        };
+    }
+    if (req.session && !req.session.save) {
+        req.session.save = (callback) => {
+            callback();
+        };
+    }
+    next();
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Collection Checker
+app.use((req, res, next) => {
+    if (collection !== null) {
+        next();
+    } else {
+        res.status(503).send();
+    }
+});
+
+passport.use(
+    new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        //callbackURL: "https://a3-ellysgorodisch.onrender.com/auth/github/redirect"
+        callbackURL: "/auth/github/redirect"
+    }, async (accessToken, refreshToken, profile, done) => {
+        console.log("Strategy:");
+        await users.findOne({githubID: profile.id}).then(async (user) => {
+            if (user) {
+                console.log("Old User:");
+                console.log(user);
+                return done(null, user);
+            } else {
+                await users.insertOne({githubID: profile.id}).then((newUser) => {
+                    console.log("New User:");
+                    console.log(newUser);
+                    return done(null, newUser.insertId);
+                });
+            }
+        });
+    })
+);
+
+function isUserAuthenticated(req, res, next) {
+    console.log(req.user);
+    if (req.user) {
+        next();
+    } else {
+        return res.send('You must login!');
+    }
+}
+
+passport.serializeUser((user, done) => {
+    console.log("Serialize:");
+    console.log(user);
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    console.log("Deserialize:");
+    console.log(user);
+    done(null, user);
+});
+
+app.get("/", (req, res) => {
+    return res.render("index.html");
+});
+
+router.get("/github", passport.authenticate("github", {
+    scope: ['user:email']
+}));
+
+router.get("/github/redirect", passport.authenticate("github", {failureRedirect: "/"}), (req, res) => {
+    console.log("Redirect:");
+    console.log(req.user);
+    if (req.user) {
+        return res.redirect("/recipes");
+    } else {
+        return res.send("You must login!");
+    }
+});
+
+router.get("/logout", (req, res) => {
+    req.logout(() => {
+        return res.redirect("/");
+    });
+});
+
+app.use("/auth", router);
+
+app.get("/recipes", isUserAuthenticated, (req, res) => {
+    console.log(req.user);
+    return res.render("recipes.html");
+});
+
+async function createTable(res, userID) {
+    /*let table = "<tr><th>Recipe Name</th><th>Meal</th><th>Prep Time</th><th>Cook Time</th><th>Total Time</th></tr>";
+    collection.find({}).toArray().then((data) => {
+        data = data.filter((a) => a.userID === userID);
+        let sort = {"Breakfast": 0, "Lunch": 1, "Dinner": 2};
+        data.sort((a, b) => sort[a.type] - sort[b.type]);
+        for (let d of data)
+            table += `<tr><td>${d.name}</td>
+                          <td>${d.type}</td>
+                          <td>${d.prep} min${d.prep == 1 ? "" : "s"}</td>
+                          <td>${d.cook} min${d.cook == 1 ? "" : "s"}</td>
+                          <td>${d.total} min${d.total == 1 ? "" : "s"}</td></tr>`;
+        res.end(table);
+    });*/
+    //console.log("createTable");
+    //console.log(userID);
+    let test = await collection.find({
+        userID: userID
+    }).toArray().then((data) => {
+        console.log(data);
+        let sort = {"Breakfast": 0, "Lunch": 1, "Dinner": 2};
+        data.sort((a, b) => sort[a.type] - sort[b.type]);
+        //console.log(data);
+        return data;
+    });
+    //console.log(test);
+    res.json(test);
+};
+
+app.get("/appdata", async (req, res) => {
+    console.log("App Data:");
+    console.log(req.user);
+    await createTable(res, req.user._id);
+});
+
+app.post("/add", express.json(), async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    await collection.insertOne({
+        userID: req.user._id,
+        name: data.name,
+        type: data.type,
+        prep: data.prep,
+        cook: data.cook,
+        total: parseInt(data.prep) + parseInt(data.cook)
+    }).then((result) => {
+        console.log("Add:");
+        console.log(result);
+    });
+    await createTable(res, req.user._id);
+});
+
+app.post("/remove", express.json(), async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    await collection.deleteOne({
+        userID: req.user._id,
+        name: {$regex: `^${data.name}$`, $options: "i"}
+    }).then(async (result) => {
+        console.log("Remove:");
+        console.log(result);
+        if (result.deletedCount === 0) {
+            res.send("Error: Recipe Does Not Exist");
+        } else {
+            await createTable(res, req.user._id);
+        }
+    });
+});
+
+app.post("/modify", express.json(), async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    await collection.updateOne(
+        {
+            userID: req.user._id,
+            name: {$regex: `^${data.name}$`, $options: "i"}
+        }, {
+            $set: {
+                type: data.type,
+                prep: data.prep,
+                cook: data.cook,
+                total: parseInt(data.prep) + parseInt(data.cook)
+            }
+        }
+    ).then(async (result) => {
+        console.log("Modify:");
+        console.log(result);
+        if (result.matchedCount === 0) {
+            res.send("Error: Recipe Does Not Exist")
+        } else {
+            await createTable(res, req.user._id);
+        }
+    });
+});
 
 ViteExpress.listen(app, 3000);
--- a4-EllysGorodisch/src/App.svelte
+++ a4-EllysGorodisch/src/App.svelte
@@ -1,55 +1,189 @@
 <script>
-  const getTodos = function () {
-    const p = fetch("/read", {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        return json;
-      });
+	const getTable = function () {
+		const p = fetch("/appdata", {
+			method: "GET",
+		})
+			.then((response) => response.json())
+			.then((table) => {
+				console.log("Refresh");
+				console.log(table);
+				return table;
+			});
+		return p;
+	};
 
-    return p;
-  };
+	const add = async function (event) {
+		event.preventDefault();
 
-  const addTodo = function (e) {
-    const todo = document.querySelector("input").value;
-    promise = fetch("/add", {
-      method: "POST",
-      body: JSON.stringify({ name: todo, completed: false }),
-      headers: { "Content-Type": "application/json" },
-    }).then((response) => response.json());
-  };
+		const nameInput = document.querySelector(".add.name"),
+			prepInput = document.querySelector(".add.prep"),
+			cookInput = document.querySelector(".add.cook"),
+			typeInput = document.querySelector(".add.type"),
+			json = {
+				name: nameInput.value,
+				prep: prepInput.value,
+				cook: cookInput.value,
+				type: typeInput.value,
+			},
+			body = JSON.stringify(json);
 
-  const toggle = function (e) {
-    fetch("/change", {
-      method: "POST",
-      body: JSON.stringify({
-        name: e.target.getAttribute("todo"),
-        completed: e.target.checked,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-  };
+		console.log(json);
 
-  let promise = getTodos();
+		if (json.name === "" || json.prep === "" || json.cook === "")
+			return alert("Please Enter All Fields");
+
+		promise = await fetch("/add", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body,
+		})
+			.then((response) => response.json())
+			.then((table) => {
+				console.log(table);
+				nameInput.value = "";
+				prepInput.value = "";
+				cookInput.value = "";
+				return table;
+			});
+	};
+
+	const remove = async function (event) {
+		event.preventDefault();
+
+		const nameInput = document.querySelector(".remove.name"),
+			json = {
+				name: nameInput.value,
+			},
+			body = JSON.stringify(json);
+
+		console.log(json);
+
+		if (json.name === "") return alert("Please Enter All Fields");
+
+		promise = await fetch("/remove", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body,
+		})
+			.then((response) => response.json())
+			.then((table) => {
+				console.log(table);
+				nameInput.value = "";
+				return table;
+			})
+			.catch(() => {
+				alert("Recipe Does Not Exist!");
+				return getTable();
+			});
+	};
+
+	const modify = async function (event) {
+		event.preventDefault();
+
+		const nameInput = document.querySelector(".modify.name"),
+			prepInput = document.querySelector(".modify.prep"),
+			cookInput = document.querySelector(".modify.cook"),
+			typeInput = document.querySelector(".modify.type"),
+			json = {
+				name: nameInput.value,
+				prep: prepInput.value,
+				cook: cookInput.value,
+				type: typeInput.value,
+			},
+			body = JSON.stringify(json);
+
+		console.log(json);
+
+		if (json.name === "" || json.prep === "" || json.cook === "")
+			return alert("Please Enter All Fields");
+
+		promise = await fetch("/modify", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body,
+		})
+			.then((response) => response.json())
+			.then((table) => {
+				console.log(table);
+				nameInput.value = "";
+				prepInput.value = "";
+				cookInput.value = "";
+				return table;
+			})
+			.catch(() => {
+				alert("Recipe Does Not Exist!");
+				return getTable();
+			});
+	};
+
+	let promise = getTable();
 </script>
 
-<input type="text" />
-<button on:click={addTodo}>add todo</button>
-
-{#await promise then todos}
-  <ul>
-    {#each todos as todo}
-      <li>
-        {todo.name} :
-        <input
-          type="checkbox"
-          todo={todo.name}
-          checked={todo.completed}
-          on:click={toggle}
-        />
-      </li>
-    {/each}
-  </ul>
-{/await}
+<div id="body">
+	<div class="container">
+		<div class="header" role="banner">
+			<h1 class="item">Recipe Cookbook</h1>
+			<button id="logout" onclick="location.href = '/auth/logout';"
+				>Logout</button>
+		</div>
+		<p class="item">
+			Recipes names are not case-senstive. Prep and cook times are in
+			minutes.
+		</p>
+		<form class="item" role="form" aria-labelledby="add-recipe">
+			<h2 id="add-recipe">Add Recipe</h2>
+			<input type="text" class="add name" placeholder="Recipe Name" />
+			<input
+				type="number"
+				class="add prep"
+				placeholder="Prep Time (min)" />
+			<input
+				type="number"
+				class="add cook"
+				placeholder="Cook Time (min)" />
+			<select class="add type" aria-label="meal">
+				<option value="Breakfast">Breakfast</option>
+				<option value="Lunch">Lunch</option>
+				<option value="Dinner">Dinner</option>
+			</select>
+			<button id="add" on:click={add}>Add</button>
+		</form>
+		<form class="item" role="form" aria-labelledby="remove-recipe">
+			<h2 id="remove-recipe">Remove Recipe</h2>
+			<input type="text" class="remove name" placeholder="Recipe Name" />
+			<button id="remove" on:click={remove}>Remove</button>
+		</form>
+		<form class="item" role="form" aria-labelledby="modify-recipe">
+			<h2 id="modify-recipe">Modify Recipe</h2>
+			<input type="text" class="modify name" placeholder="Recipe Name" />
+			<input
+				type="number"
+				class="modify prep"
+				placeholder="Prep Time (min)" />
+			<input
+				type="number"
+				class="modify cook"
+				placeholder="Cook Time (min)" />
+			<select class="modify type" aria-label="meal">
+				<option value="Breakfast">Breakfast</option>
+				<option value="Lunch">Lunch</option>
+				<option value="Dinner">Dinner</option>
+			</select>
+			<button id="modify" on:click={modify}>Modify</button>
+		</form>
+	</div>
+	{#await promise then table}
+		<table role="main">
+			<tr>
+				<th>Recipe Name</th><th>Meal</th><th>Prep Time</th><th
+					>Cook Time</th
+				><th>Total Time</th>
+			</tr>
+			{#each table as row}
+				<tr
+					><td>{row.name}</td><td>{row.type}</td><td>{row.prep}</td
+					><td>{row.cook}</td><td>{row.total}</td></tr>
+			{/each}
+		</table>
+	{/await}
+</div>
