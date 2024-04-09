1e755b82ada52e3c5f427aa4c1045703be4db456
TheDarkThatBarks <125921991+TheDarkThatBarks@users.noreply.github.com>
2024-04-09 11:40:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -80,8 +80,8 @@ passport.use(
     new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        //callbackURL: "https://a4-ellysgorodisch.onrender.com/auth/github/redirect"
-        callbackURL: "/auth/github/redirect"
+        callbackURL: "https://a4-ellysgorodisch.onrender.com/auth/github/redirect"
+        //callbackURL: "/auth/github/redirect"
     }, async (accessToken, refreshToken, profile, done) => {
         console.log("Strategy:");
         await users.findOne({githubID: profile.id}).then(async (user) => {
