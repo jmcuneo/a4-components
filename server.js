1ecc2216dcbb2fb04301e5241acdb97b4dbee0bd
TheDarkThatBarks <125921991+TheDarkThatBarks@users.noreply.github.com>
2024-04-09 12:28:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -187,9 +187,9 @@ app.get("/appdata", async (req, res) => {
     await createTable(res, req.user._id);
 });
 
-app.get("/App.svelte", (req, res) => {
+app.get("/assets/nested-Dy8E001p.js", (req, res) => {
     res.setHeader('Content-Type', 'text/javascript');
-    res.sendFile("src/App.svelte");
+    res.sendFile("dist/assets/nested-Dy8E001p.js");
 });
 
 app.post("/add", express.json(), async (req, res) => {
