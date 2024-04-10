aede273a8cc8c2965276f51a441899f4eecc02fb
aniginger <cspgithub@twinleaf.xyz>
2024-04-10 00:15:36 -0400

    Fixed server

--- vite-project/server.js
+++ vite-project/server.js
@@ -1,26 +1,9 @@
-import express from  'express'
-import ViteExpress from 'vite-express'
+//e.g server.js
+import express from "express";
+import ViteExpress from "vite-express";
 
-const app = express()
+const app = express();
 
-const todos = [
-  { name:'buy groceries', completed:false }
-]
+app.get("/message", (_, res) => res.send("Hello from express!"));
 
-app.use( express.json() )
-
-app.get( '/read', ( req, res ) => res.json( todos ) )
-
-app.post( '/add', ( req,res ) => {
-  todos.push( req.body )
-  res.json( todos )
-})
-
-app.post( '/change', function( req,res ) {
-  const idx = todos.findIndex( v => v.name === req.body.name )
-  todos[ idx ].completed = req.body.completed
-  
-  res.sendStatus( 200 )
-})
-
-ViteExpress.listen( app, 3000 )
+ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,61 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const Todo = props => (
-  <li>{props.name} : 
-    <input type="checkbox" defaultChecked={props.completed} onChange={ e => props.onclick( props.name, e.target.checked ) }/>
-  </li>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
-
-const App = () => {
-  const [todos, setTodos] = useState([ ]) 
-
-  function toggle( name, completed ) {
-    fetch( '/change', {
-      method:'POST',
-      body: JSON.stringify({ name, completed }),
-      headers: { 'Content-Type': 'application/json' }
-    })
-  }
-
-  function add() {
-    const value = document.querySelector('input').value
-
-    fetch( '/add', {
-      method:'POST',
-      body: JSON.stringify({ name:value, completed:false }),
-      headers: { 'Content-Type': 'application/json' }
-    })
-    .then( response => response.json() )
-    .then( json => {
-       setTodos( json )
-    })
-  }
-  
-  // make sure to only do this once
-  if( todos.length === 0 ) {
-    fetch( '/read' )
-      .then( response => response.json() )
-      .then( json => {
-        setTodos( json ) 
-      })
-  }
-    
-  useEffect(()=> {
-    fetch( '/read' )
-      .then( response => response.json() )
-      .then( json => {
-        setTodos( json ) 
-      })
-  }, [] )
-
-  return (
-    <div className="App">
-    <input type='text' /><button onClick={ e => add()}>add</button>
-      <ul>
-        { todos.map( (todo,i) => <Todo key={i} name={todo.name} completed={todo.completed} onclick={ toggle } /> ) }
-     </ul> 
-    </div>
-  )
-}
-
-export default App
