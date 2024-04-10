4fb693ff6e164b35f013374833ee828904282b4b
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2024-04-10 15:09:54 -0400

    Re-implemented most HTML

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
-import './App.css';
+import './main.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+          <h1>Smalltown Arcade Login</h1>
+
+          <form id ="userlogin">
+            <input type="text" id="email" placeholder="email" />
+            <input  type="text" id="password" placeholder="password" />
+            <button id ="loginButton">Login</button>
+          </form>
+
+
+          <h2>Your High Scores:</h2>
+
+          <form id ="userinput">
+            <input type="text" id="score" placeholder="score" />
+            <input type="text" id="playtime" placeholder="playtime" />
+            <input type="text" id="date" placeholder="date (unique id)" />
+
+            <button id = "dataButton" >Add/Update</button>
+            <button id = "deleteButton" >Delete</button>
+          </form>
+
+
+
+          <table id ="InformationTable">
+            <tr>
+              <th>Score</th>
+              <th>Playtime</th>
+              <th>Score / Time</th>
+              <th id ="idBlock">Date</th>
+
+            </tr>
+
+          </table>
+
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
