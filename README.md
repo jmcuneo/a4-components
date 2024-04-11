19c4b93349287276720d81820740d4233a18a0cc
Lehong-Wang <lwang11@wpi.edu>
2024-04-11 17:50:40 -0400

    clean up

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ your hosting link e.g. http://a4-charlieroberts.glitch.me
 
 Include a very brief summary of your project here and what you changed / added to assignment #3. Briefly (3–4 sentences) answer the following question: did the new technology improve or hinder the development experience?
 
-Unlike previous assignments, this assignment will be solely graded on whether or not you successfully complete it. Partial credit will be generously given.
+I used the svelte framwork to improve the client code, and used vite + express for the server side. The svelte framwork is very wield at first, and hard to develop within, but once I got some understanding of how it works, it's pretty straightforward. The way it brings css, js, and html all into one file makes the development easier.
--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,149 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
+    // const express = require("express"),
+    //     app = express()
+
+    // // app.use(express.static("public") )
+    // app.use(express.json() )
+    // // require('dotenv').config();
+
+    const getEntry = function() {
+        const p = fetch( '/read', {
+            method:'GET' 
+        })
+        .then( response => response.json() )
+        .then( json => {
+            console.log(json)
+            return json 
+        })
+    
+        return p
+    }
+  
+    const addEntry = function( e ) {
+        // console.log(e)
+        // console.log(e.target.value)
+        const input_name = document.getElementById("user_name").value
+        const input_pass = document.getElementById("password").value
+        const input_ready = document.getElementById("ready").checked
+        const input_age = parseInt(document.getElementById("age").value)
+        promise = fetch( '/add', {
+            method:'POST',
+            body: JSON.stringify({ 
+                "user": input_name,
+                "password": input_pass,
+                "ready": input_ready,
+                "age": input_age}),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then( response => response.json() )
+    }
+  
+    const toggle = function( e ) {
+
+        fetch( '/change', {
+            method:'POST',
+            body: JSON.stringify({ name:e.target.getAttribute('todo'), completed:e.target.checked }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+    }
+  
+    const remove_entry = (e) => {
+        e.preventDefault()
+        console.log(`Removing Entry ${e.target.value}`)
+        promise = fetch( '/remove', {
+            method:'POST',
+            body: JSON.stringify({"idx": parseInt(e.target.value)}),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then( response => response.json() )
+
+        // getEntry()
+    }
+
+
+    let promise = getEntry()
+
+    // const 
+
+
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<p class="p_class">USER DATA FORM</p>
 
-  <div class="card">
-    <Counter />
-  </div>
+<form id="input_form">
+    <br/><label>User Name:</label><br>
+    <input type="text" id="user_name" placeholder="User Name" value="Default_Name">
+    <br/><label>Password:</label><br>
+    <input type="password" id="password" value="Default_Password" placeholder="Password">
+    <br/><label>Ready:</label><br>
+    <input type="checkbox" id="ready" value="True">
+    <br/><label>Age:</label><br>
+    <input type="number" id="age" value=0>
+    <br/>
+    <!-- <button id="add_to_db_button">Add</button> -->
+</form>
+<br/>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+<!-- <input type='text' /> -->
+<button on:click={addEntry} value={"hi"}>Submit</button>
+<br/><br/><br/><br/>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+
+<!-- {#await promise then todos}
+    <ul>
+
+    {#each todos as todo}
+    <li>{todo.name} : <input type='checkbox' value={todo.name} checked={todo.completed} on:click={toggle}></li>
+    {/each}
+
+    </ul>
+{/await} -->
+
+
+
+{#await promise then data_list}
+    <!-- <ul>
+
+    {#each data_list as data, idx}
+    <li>{data.user} : <button on:click={remove_entry} value={idx}>Remove</button></li>
+    {/each}
+
+    </ul> -->
+
+    <table>
+        <!-- {#each Object.entries(data_list[0]) as [key, value]}
+            <td></td>
+
+        {/each} -->
+        <tr>
+            <td>User Name</td>
+            <td>Password</td>
+            <td>Ready?</td>
+            <td>Age</td>
+            <td>Remove?</td>
+        </tr>
+        {#each data_list as data, idx}
+            <tr>
+            {#each Object.entries(data) as [key, value]}
+                <td>{value}</td>
+            {/each}
+            <td><button on:click={remove_entry} value={idx}>Remove</button></td>
+
+            </tr>
+        {/each}
+      </table>
+
+{/await}
+
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    td {
+      padding: 20px;
+      background-color:gray;
+    }
+    button {
+        color: blue;
+        background-color:aquamarine;
+    }
 </style>
