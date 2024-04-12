c6a0dc3a9c64ef6a5abba802d68700eb467122fd
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2024-04-11 23:01:13 -0400

    ho

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const { PORT = 3001 } = process.env;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+    base: '/', 
+  server: {
+ 
+  },
+  build: {
+    outDir: 'dist/app',
+  },
+});
